019415b65ddf58a5de4065cac68174b6b210e358
dylan tran <dytran@student.unimelb.edu.au>
2025-03-26 17:52:57 +1100

    Updated Submission Class

--- app/src/main/java/com/unimelb/swen30006/workshops/Submission.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Submission.java
@@ -1,7 +1,11 @@
 package com.unimelb.swen30006.workshops;
 
 // Sample private submission class to be replaced by your implementation
-class Submission {
+    class   Submission {
+    private int attemptNum;
+    private File[] files;
+    private int numFiles;
+
     // Return two fake files
     public File[] includedFiles(){
         File[] files = new File[2];
